8d209378a7ce984a1f4f2bd74c7da8abbe1583ab
Felipe Cuervo <pipecod32145@gmail.com>
2023-04-17 17:08:40 -0500

    helper and json server

---
+++ src/app/helpers/helpHttp.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const helpHttp = () => {
+
+ const customFetch = (url, options)=>{
+    const defaultHEader ={
+        accept:"application/json"
+    };
+    const controller = new AbortController();
+    options.signal = controller.signal
+
+    options.method = options.method || "GET"
+    options.headers = options.headers
+    ?{...defaultHEader,...options.headers}
+    :defaultHEader
+    options.body = JSON.stringify(options.body) || false
+    if(!options.body) delete options.body
+
+    console.log(options)
+    setTimeout(()=>controller.abort(),3000)
+
+    return fetch(url)
+    .then(res=>res.ok ? res.json():Promise.reject({
+        err:true,
+        status: res.status || "00",
+        statusText: res.statusText || "Error inesperado"
+    }))
+    .catch(err=>err)
+ }
+ const get = (url,options = {})=> customFetch(url,options)
+ const post = (url,options = {})=>{
+    options.method = "POST"
+    return customFetch(url,options)
+ }
+ const put = (url,options = {})=>{
+    options.method = "PUT"
+    return customFetch(url,options)
+ }
+ const del = (url,options = {})=>{
+    options.method = "DELETE"
+    return customFetch(url,options)
+ }
+
+  return {
+    get,post,put,del
+  }
+}
+
+export default helpHttp
